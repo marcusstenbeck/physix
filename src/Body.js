ad82af7186b0e9a4e8571f175a185dac89f24b87
marcusstenbeck <marcus.stenbeck@gmail.com>
2014-02-05 06:53:04 +0100

    Add basic body members. Add force accumulator.

--- src/Body.js
+++ src/Body.js
@@ -1,5 +1,20 @@
 define([
+	'./Vec2'
 ], function(
+	Vec2
 ) {
-	// Body.js
-};
+	function Body() {
+		this.mass = 1;
+		this.pos = new Vec2();
+		this.vel = new Vec2();
+		this.acc = new Vec2();
+		this.accumulatedForce = new Vec2();
+	}
+
+	Body.prototype.applyForce = function(vecForce) {
+		this.accumulatedForce.x += vecForce.x;
+		this.accumulatedForce.y += vecForce.y;
+	};
+
+	return Body;
+});
