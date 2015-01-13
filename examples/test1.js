fd13df884ddb7edec6d44d0015b6453a4ee7e88a
marcusstenbeck <marcus.stenbeck@gmail.com>
2015-01-13 19:24:26 +0100

    Rewrite test1 to contain 4 physics worlds.

--- examples/test1.js
+++ examples/test1.js
@@ -11,83 +11,169 @@ requirejs.config({
     }
 });
 require([
-	'World',
-	'Body',
-	'Vec2'
+    'World',
+    'Body',
+    'Vec2'
 ], function(
-	World,
-	Body,
-	Vec2
+    World,
+    Body,
+    Vec2
 ) {
-	var w;
+    var worlds = [];  // hold physics worlds
 
-	// TODO: Move this someplace better
-	window.requestAnimFrame =
-	    window.requestAnimationFrame ||
-	    window.webkitRequestAnimationFrame ||
-	    window.mozRequestAnimationFrame ||
-	    window.oRequestAnimationFrame ||
-	    window.msRequestAnimationFrame ||
-	    function(callback) {
-	        window.setTimeout(callback, 1000 / 60);
-	};
+    // TODO: Move this someplace better
+    window.requestAnimFrame =
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(callback) {
+            window.setTimeout(callback, 1000 / 60);
+    };
 
-	var canvas = document.getElementById('c');
-	var ctx = canvas.getContext('2d');
+    var canvas = document.getElementById('c');
+    var ctx = canvas.getContext('2d');
 
-	function resize() {
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
-	}
+    function resize() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
 
-	var dt = 0, lastTime = 0;
-	function update(time) {
-		requestAnimFrame(update);
+    var dt = 0, lastTime = 0;
+    function update(time) {
+        requestAnimFrame(update);
 
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		dt = Math.min(60, time - lastTime);
+        dt = Math.min(60, time - lastTime);
 
-		w.update(dt);
-		var bodies = w.bodies;
-		for(var i = 0; i < bodies.length; i++) {
-			var bound = bodies[i].getBounds();
-			ctx.beginPath();
-			ctx.strokeStyle = "magenta";
-			ctx.strokeRect(bound.left, canvas.height - bound.top, bodies[i].shape.width, bodies[i].shape.height);
-			ctx.restore();
-		}
+        worlds.forEach(function(world) {
+            world.update(dt);
 
-		lastTime = time;
-	}
+            var bodies = world.bodies;
+            for(var i = 0; i < bodies.length; i++) {
+                var bound = bodies[i].getBounds();
+                ctx.beginPath();
+                ctx.strokeStyle = "magenta";
+                ctx.strokeRect(bound.left, canvas.height - bound.top, bodies[i].shape.width, bodies[i].shape.height);
+                ctx.restore();
+            }
+        });
 
-	function init() {
-		resize();
-		window.addEventListener('resize', resize);
+        lastTime = time;
+    }
 
-		w = new World();
-		w.gravity.y = -0.001;
+    function init() {
+        resize();
+        window.addEventListener('resize', resize);
 
-		var b;
-		for(var i = 0; i < 3; i++) {
-			b = new Body();
-			b.pos.x = 0.5*canvas.width + i*20 - 20;
-			b.pos.y = 0.55*canvas.height + 0.3*i*canvas.height/2;
-			w.bodies.push(b);
-		}
+        var w;
 
-		w.bodies[1].shape.height = 20;
+        // ---------------------------------
 
-		b = new Body({
-			type: Body.KINEMATIC
-		});
-		b.pos.x = 0.5*canvas.width;
-		b.pos.y = 0.5*canvas.height;
-		b.shape.width = 100;
-		w.bodies.push(b);
+        w = new World();
+        w.gravity.y = -0.001;
 
-		update();
-	}
+        var b;
+        for(var i = 0; i < 3; i++) {
+            b = new Body();
+            b.pos.x = 0.5*canvas.width + i*20 - 20;
+            b.pos.y = 0.55*canvas.height + 0.3*i*canvas.height/2;
+            w.bodies.push(b);
+        }
 
-	init();
+        w.bodies[1].shape.height = 20;
+
+        b = new Body({
+            type: Body.KINEMATIC
+        });
+        b.pos.x = 0.5*canvas.width;
+        b.pos.y = 0.5*canvas.height;
+        b.shape.width = 100;
+        w.bodies.push(b);
+
+        worlds.push(w);
+
+        // ----------------------------------
+
+        w = new World();
+        w.gravity.y = 0.001;
+
+        var b;
+        for(var i = 0; i < 3; i++) {
+            b = new Body();
+            b.pos.x = 0.5*canvas.width - i*20 + 20;
+            b.pos.y = 0.45*canvas.height - 0.3*i*canvas.height/2;
+            w.bodies.push(b);
+        }
+
+        w.bodies[1].shape.height = 20;
+
+        b = new Body({
+            type: Body.KINEMATIC
+        });
+        b.pos.x = 0.5*canvas.width;
+        b.pos.y = 0.5*canvas.height;
+        b.shape.width = 100;
+        w.bodies.push(b);
+
+        worlds.push(w);
+
+        // -----------------------------------
+
+        w = new World();
+        w.gravity.x = -0.001;
+
+        var b;
+        for(var i = 0; i < 3; i++) {
+            b = new Body();
+            b.pos.x = 0.55*canvas.width + 0.3*i*canvas.width/2;
+            b.pos.y = 0.5*canvas.height + i*20 - 20;
+            w.bodies.push(b);
+        }
+
+        w.bodies[1].shape.width = 20;
+
+        b = new Body({
+            type: Body.KINEMATIC
+        });
+        b.pos.x = 0.5*canvas.width;
+        b.pos.y = 0.5*canvas.height;
+        b.shape.height = 100;
+        w.bodies.push(b);
+
+        worlds.push(w);
+
+        // --------------------------------------
+
+        w = new World();
+        w.gravity.x = 0.001;
+
+        var b;
+        for(var i = 0; i < 3; i++) {
+            b = new Body();
+            b.pos.x = 0.45*canvas.width - 0.3*i*canvas.width/2;
+            b.pos.y = 0.5*canvas.height - i*20 + 20;
+            w.bodies.push(b);
+        }
+
+        w.bodies[1].shape.width = 20;
+
+        b = new Body({
+            type: Body.KINEMATIC
+        });
+        b.pos.x = 0.5*canvas.width;
+        b.pos.y = 0.5*canvas.height;
+        b.shape.height = 100;
+        w.bodies.push(b);
+
+        worlds.push(w);
+
+        // --------------------------------------
+
+        update();
+    }
+
+    init();
 });
